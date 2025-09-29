d1c9eca4d34d552a658c068872a126e72a05479e
vannnnnn <134368005+BUTTERCUPXCZ@users.noreply.github.com>
2025-09-29 13:03:31 +0800

    fix error

--- frontend/src/components/ui/label.tsx
+++ frontend/src/components/ui/label.tsx
@@ -1,6 +1,5 @@
 import * as React from "react"
 import * as LabelPrimitive from "@radix-ui/react-label"
-<<<<<<< HEAD
 
 import { cn } from "@/lib/utils"
 
@@ -21,27 +20,3 @@ function Label({
 }
 
 export { Label }
-=======
-import { cva, type VariantProps } from "class-variance-authority"
-
-import { cn } from "@/lib/utils"
-
-const labelVariants = cva(
-  "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-)
-
-const Label = React.forwardRef<
-  React.ElementRef<typeof LabelPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root> &
-    VariantProps<typeof labelVariants>
->(({ className, ...props }, ref) => (
-  <LabelPrimitive.Root
-    ref={ref}
-    className={cn(labelVariants(), className)}
-    {...props}
-  />
-))
-Label.displayName = LabelPrimitive.Root.displayName
-
-export { Label }
->>>>>>> 9e87d61931cdbbfe9c8a9c1caf1303bdcae5e723
